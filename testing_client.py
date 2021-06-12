6a4fbef3d395fe63100d615a219296d013556019
Daniel Fischer <danielfm123@gmail.com>
2021-06-12 12:16:34 -0400

    finished

--- testing_client.py
+++ testing_client.py
@@ -4,10 +4,9 @@ password = 'Z_eIKSZvC9oCjf-ocg27Q070OAzCiw6LH7892okyzsY='
 server = 'http://127.0.0.1:8081'
 
 self = KeyVaultClient(server,password)
-self.get_value('nombre')
+
+self.set_value('daniel','fischer')
+self.set_value('marta','stolarska')
+self.get_value('marta')
+self.get_value('daniel')
 self.get_value('asd')
-
-self.set_value('otro','pene')
-self.get_value('otro')
-self.rm_value('otro')
-
