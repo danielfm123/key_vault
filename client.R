d6eb91c1e3674f74cb06a4fddd68226908cc68f8
Daniel Fischer <danielfm123@gmail.com>
2021-06-13 23:52:38 -0400

    R

---
+++ client.R
@@ -0,0 +1,12 @@
+library(reticulate)
+source_python('client.py')
+
+password = 'Z_eIKSZvC9oCjf-ocg27Q070OAzCiw6LH7892okyzsY='
+server = 'http://127.0.0.1:8081'
+
+self = KeyVaultClient(server,password)
+self$set_value('values','daniel','fischer')
+self$set_value('values','marta','stolarska')
+self$get_value('values','marta')
+self$get_value('values','daniel')
+self$get_value('values','asd')
